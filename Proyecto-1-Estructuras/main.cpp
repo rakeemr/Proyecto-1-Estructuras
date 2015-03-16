5936742c2de461012cd559861a38811464b3b447
Ginko-san <21salchi21@gmail.com>
2015-03-16 00:04:28 -0600

    Casi Nada
    Yo trabajo lista de personas Doblemente enlazada
    aparatos electricos lista circular
    
    sub listas
    
    Localidad-sector
    localidad-vivienda
    personas-consumo

--- Proyecto-1-Estructuras/main.cpp
+++ Proyecto-1-Estructuras/main.cpp
@@ -6,8 +6,46 @@ using namespace std;
 */
 ///------------------------------------------------------------------------------------------------------------///
 
+struct Persons{
+    int id;
+    string name;
+    string lastName;
+    int age;
+    struct Persons *next;
+    struct Persons *bef;
+    Persons(int i, string n, string l, int a){
+        id = i;
+        name = n;
+        lastName = l;
+        age = a;
+        next = NULL;
+        bef = NULL;
+    }
+};
+
+struct electricalDevices{
+    double kilowhatts;
+    struct electricalDevices *next;
+    electricalDevices(double k){
+        kilowhatts = k;
+        next = NULL;
+    }
+};
+
+struct localitySector{
+
+};
+
+struct localityHome{
+
+};
+
+struct personConsumption{
+
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
     return 0;
 }
