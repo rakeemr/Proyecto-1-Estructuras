7f4d9bf8cacfbc7457e92e6a1aeff519d72f5996
rakeemr <raqe-11@hotmail.com>
2015-03-22 12:57:07 -0600

    Comienzo de las inserciones

--- Proyecto-1-Estructuras/main.cpp
+++ Proyecto-1-Estructuras/main.cpp
@@ -2,9 +2,29 @@
 
 using namespace std;
 ///---------------------------------------------INFORMACION-----------------------------------------------------///
-/*Fecha de inicio: 09 de marzo
+/*
+Fecha de inicio: 09 de marzo
+Creadores: Raquel Mora Rojas
+           Andrés García Salas
+
+Hora de inicio: 4:30 pm
 */
 ///------------------------------------------------------------------------------------------------------------///
+
+struct Persona {
+    string nombre;
+    int cedula;
+    string apellidos;
+    struct Persona *sig;
+    Persona(string nombre, int cedula, string apellidos){
+        nombre = n;
+        cedula = c;
+        apellidos = a;
+        sig = NULL;
+    }
+}*primeroP;
+
+
 
 int main()
 {
