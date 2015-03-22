008d82536b97aed04e51c1c978f4508ebdc59701
rakeemr <raqe-11@hotmail.com>
2015-03-22 15:14:51 -0600

    Estructuras completas
    Arregle unos errores y comentarios, además agregue todos los enlaces y
    estructuras faltantes.
    
    Igual hay que revisar si esta bien.

--- Proyecto-1-Estructuras/main.cpp
+++ Proyecto-1-Estructuras/main.cpp
@@ -1,32 +1,16 @@
 #include <iostream>
 
 using namespace std;
-///---------------------------------------------INFORMACION-----------------------------------------------------///
+///---------------------------------------------INFORMATION-----------------------------------------------------///
 /*
-Fecha de inicio: 09 de marzo
-Creadores: Raquel Mora Rojas
-           Andrés García Salas
+Starting Date: March 09
+Creators: Raquel Mora Rojas
+          Andrés Garcia Salas
 
-Hora de inicio: 4:30 pm
+Begin Hour: 4:30 pm
 */
-///------------------------------------------------------------------------------------------------------------///
+///-------------------------------------Declaration of the structures-----------------------------------------///
 
-<<<<<<< HEAD
-struct Persona {
-    string nombre;
-    int cedula;
-    string apellidos;
-    struct Persona *sig;
-    Persona(string nombre, int cedula, string apellidos){
-        nombre = n;
-        cedula = c;
-        apellidos = a;
-        sig = NULL;
-    }
-}*primeroP;
-
-
-=======
 struct Persons{
     int id;
     string name;
@@ -42,29 +26,84 @@ struct Persons{
         next = NULL;
         bef = NULL;
     }
-};
+}*firstP;
 
 struct electricalDevices{
-    double kilowhatts;
+    double kilowatts;
     struct electricalDevices *next;
     electricalDevices(double k){
-        kilowhatts = k;
+        kilowatts = k;
+        next = NULL;
+    }
+}*firstED;
+
+struct Sector {
+    string type;
+    string name;
+    string trend;
+    int percent;
+    struct Sector * next;
+    Sector(string type, string name, string trend, int percent){
+        type = t;
+        name = n;
+        trend = tr;
+        percent = p;
+        next = NULL;
+    }
+}*firstS;
+
+struct Energy{
+    string name;
+    int usagePercentage;
+    struct Energy * next;
+    Energy(string name, int usagePercentage){
+        name = n;
+        usagePercentage = u;
+        next = NULL;
+    }
+}*firstE;
+
+struct Locality{
+    string name;
+    double energyIntake;
+    struct Locality *next;
+    struct Home * linkH;
+    struct localitySector * linkLS;
+    Locality(string name, double energyIntake){
+        name = n;
+        energyIntake = e;
+        next = NULL;
+    }
+}*firstL;
+
+struct Home{
+    string code;
+    string address;
+    struct Home * next;
+    struct Intake * linkI;
+    Home(string code, string address){
+        code = c;
+        address = a;
         next = NULL;
     }
 };
 
+struct Intake{
+    struct Intake * next;
+    struct electricalDevices * linkED;
+    struct Energy * linkE;
+    struct Sector * linkS;
+};
+
 struct localitySector{
+    struct Sector * linkS;
+    struct localitySector * next;
+}*firstLS;
 
-};
-
-struct localityHome{
-
-};
-
-struct personConsumption{
-
-};
->>>>>>> origin/master
+struct homePersons{
+    struct Persons * linkP;
+    struct homePersons * next;
+}*firstHP;
 
 int main()
 {
